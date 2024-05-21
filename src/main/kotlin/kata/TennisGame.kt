c083ac603fbc332e4b3056956faff4fc0bcb5367
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-05-21 16:22:18 +0200

    Refactor TennisGame: use variable for players points

--- src/main/kotlin/kata/TennisGame.kt
+++ src/main/kotlin/kata/TennisGame.kt
@@ -5,19 +5,21 @@ data class TennisGame(val pointsPlayerOne: Int = 0, val pointsPlayerTwo: Int = 0
     companion object {
 
         fun playerOneWinsPoint(game: TennisGame): TennisGame {
+            val playersOnePoints = game.pointsPlayerOne.inc()
             if (game.pointsPlayerOne == 0)
-                return TennisGame(game.pointsPlayerOne.inc(), 0, "15-Love")
+                return TennisGame(playersOnePoints, 0, "15-Love")
             else if (game.pointsPlayerOne == 1)
-                return TennisGame(game.pointsPlayerOne.inc(), 0, "30-Love")
-            else return TennisGame(game.pointsPlayerOne.inc(), 0, "40-Love")
+                return TennisGame(playersOnePoints, 0, "30-Love")
+            else return TennisGame(playersOnePoints, 0, "40-Love")
         }
 
         fun playerTwoWinsPoint(game: TennisGame): TennisGame {
+            val playersTwoPoints = game.pointsPlayerTwo.inc()
             if (game.pointsPlayerTwo == 0)
-                return TennisGame(0, game.pointsPlayerTwo.inc(), "Love-15")
+                return TennisGame(0, playersTwoPoints, "Love-15")
             else if (game.pointsPlayerTwo == 1)
-                return TennisGame(0, game.pointsPlayerTwo.inc(), "Love-30")
-            return TennisGame(0, game.pointsPlayerTwo.inc(), "Love-40")
+                return TennisGame(0, playersTwoPoints, "Love-30")
+            return TennisGame(0, playersTwoPoints, "Love-40")
         }
     }
 }
