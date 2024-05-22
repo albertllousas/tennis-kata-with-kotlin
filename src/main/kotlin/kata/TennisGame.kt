6d073387cc82da7dfd80d82bc58ccd463352dd35
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-05-22 11:55:26 +0200

    Refactor TennisGame: convert when to expression body

--- src/main/kotlin/kata/TennisGame.kt
+++ src/main/kotlin/kata/TennisGame.kt
@@ -10,15 +10,13 @@ data class TennisGame(val pointsPlayerOne: Int = 0, val pointsPlayerTwo: Int = 0
         fun playerTwoWinsPoint(game: TennisGame): TennisGame =
             playerWinsPoint(game.pointsPlayerOne, game.pointsPlayerTwo.inc())
 
-        private fun playerWinsPoint(p1: Int, p2: Int): TennisGame {
-            return when {
-                p1 >= 4 && p1 >= p2 + 2 -> TennisGame(p1, p2, "Player one wins")
-                p2 >= 4 && p2 >= p1 + 2 -> TennisGame(p1, p2, "Player two wins")
-                p1 >= 3 && p2 >= 3 && p1 == p2 -> TennisGame(p1, p2, "Deuce")
-                p1 >= 3 && p2 >= 3 && p1.dec() == p2 -> TennisGame(p1, p2, "Advantage player one")
-                p1 >= 3 && p2 >= 3 && p1 == p2.dec() -> TennisGame(p1, p2, "Advantage player two")
-                else -> TennisGame(p1, p2, "${toScore(p1)}-${toScore(p2)}")
-            }
+        private fun playerWinsPoint(p1: Int, p2: Int): TennisGame = when {
+            p1 >= 4 && p1 >= p2 + 2 -> TennisGame(p1, p2, "Player one wins")
+            p2 >= 4 && p2 >= p1 + 2 -> TennisGame(p1, p2, "Player two wins")
+            p1 >= 3 && p2 >= 3 && p1 == p2 -> TennisGame(p1, p2, "Deuce")
+            p1 >= 3 && p2 >= 3 && p1.dec() == p2 -> TennisGame(p1, p2, "Advantage player one")
+            p1 >= 3 && p2 >= 3 && p1 == p2.dec() -> TennisGame(p1, p2, "Advantage player two")
+            else -> TennisGame(p1, p2, "${toScore(p1)}-${toScore(p2)}")
         }
 
         private fun toScore(points: Int) = when (points) {
